a279c8ba71cd21b19257ade15ac53bbc9a722e52
Cesar Cordoba <cordobacesar29@gmail.com>
2024-02-16 16:13:25 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home, About, Contact, Proyects } from "./pages";
+function App() {
+  return (
+    <main className="bg-slate-300/20">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/proyects" element={<Proyects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import { Home } from "./Home";
+import { About } from "./About";
+import { Proyects } from "./Proyects";
+import { Contact } from "./Contact";
+
+export {Home, About, Contact, Proyects}
