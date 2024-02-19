4a51f502051bef7c3d50981bf18fb7f8edb84279
Cesar Cordoba <cordobacesar29@gmail.com>
2024-02-19 17:47:19 -0300

    model index

---
+++ src/models/index.tsx
@@ -0,0 +1,12 @@
+//@ts-ignore
+import Bird from "./Bird";
+//@ts-ignore
+import Fox from "./Fox";
+//@ts-ignore
+import Island from "./Island";
+//@ts-ignore
+import Plane from "./Plane";
+//@ts-ignore
+import Sky from "./Sky";
+
+export { Bird, Fox, Island, Plane, Sky };
